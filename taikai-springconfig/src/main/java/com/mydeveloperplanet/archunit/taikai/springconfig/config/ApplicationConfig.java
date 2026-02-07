8de56a33a72bf0f97739398349bf8078d76e5f25
mydeveloperplanet <gunter@mydeveloperplanet.com>
2026-02-07 11:24:30 +0100

    Spring config is also fixed

--- taikai-springconfig/src/main/java/com/mydeveloperplanet/archunit/taikai/springconfig/config/ApplicationConfig.java
+++ taikai-springconfig/src/main/java/com/mydeveloperplanet/archunit/taikai/springconfig/config/ApplicationConfig.java
@@ -1,7 +1,9 @@
 package com.mydeveloperplanet.archunit.taikai.springconfig.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties("app")
+//@Configuration
 public record ApplicationConfig(boolean enabled, String remoteServer, int remotePort) {
 }
