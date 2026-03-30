3f0cd0df8c21a9d5190ffaf9e93470755922ccf7
mydeveloperplanet <gunter@mydeveloperplanet.com>
2026-03-30 19:25:54 +0200

    Record instead of Lombok

--- taikai-dependency/src/main/java/com/mydeveloperplanet/archunit/taikai/dependency/model/Customer.java
+++ taikai-dependency/src/main/java/com/mydeveloperplanet/archunit/taikai/dependency/model/Customer.java
@@ -10,34 +10,7 @@ public class Customer {
     private String firstName;
     private String lastName;
 
-    // Without Lombok I will need these
-//    public Customer(Long id, String firstName, String lastName) {
-//        this.id = id;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
+    // Without Lombok, just use a Record
+//    public record(Long id, String firstName, String lastName) {}
+
 }
